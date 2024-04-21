a0fa5f3a663adf7162ce8e9a1356d77bac4eb186
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-21 20:52:53 +0900

    feat: .env init

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:restaurant_app/user/view/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // env init
+  await dotenv.load(fileName: ".env");
   runApp(
     _App(),
   );
