86d2bc6e24429bd493dd0d9780be5cbce4d2b415
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-21 21:33:13 +0900

    feat: 토큰 검증 로직 추가

---
+++ app/lib/common/view/splash_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/common/const/colors.dart';
+import 'package:restaurant_app/common/const/data.dart';
+import 'package:restaurant_app/common/layout/default_layout.dart';
+import 'package:restaurant_app/common/view/root_tab.dart';
+import 'package:restaurant_app/user/view/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    checkToken();
+  }
+
+  Future<void> checkToken() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    if (refreshToken == null || accessToken == null) {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+        (route) => false,
+      );
+    } else {
+      // TODO :: token 검증
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (_) => const RootTab()),
+        (route) => false,
+      );
+    }
+  }
+
+  Future<void> deleteToken() async {
+    await storage.deleteAll();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+        backgroundColor: PRIMARY_COLOR,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "asset/img/logo/logo.png",
+                width: MediaQuery.of(context).size.width / 2,
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              const CircularProgressIndicator(
+                color: Colors.white,
+              )
+            ],
+          ),
+        ));
+  }
+}
