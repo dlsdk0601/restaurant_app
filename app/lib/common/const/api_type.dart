4a7fa290cfc3860bddc7627eaeb2e4210eeca110
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-23 00:28:22 +0900

    feat: 레스토랑 list 연동

--- app/lib/common/const/api_type.dart
+++ app/lib/common/const/api_type.dart
@@ -4,3 +4,27 @@ class SignInRes {
 
   SignInRes({required this.refreshToken, required this.accessToken});
 }
+
+class RestaurantListResItem {
+  final String id; // "1952a209-7c26-4f50-bc65-086f6e64dbbd",
+  final String name; // "우라나라에서 가장 맛있는 짜장면집",
+  final String thumbUrl; // "/img/thumb.png",
+  final List<String> tags; // ["신규","세일중"],
+  final String priceRange; // "cheap",
+  final double ratings; // 4.89,
+  final int ratingsCount; // 200,
+  final int deliveryTime; // 20,
+  final int deliveryFee; // 3000
+
+  RestaurantListResItem({
+    required this.id,
+    required this.name,
+    required this.thumbUrl,
+    required this.tags,
+    required this.priceRange,
+    required this.ratings,
+    required this.ratingsCount,
+    required this.deliveryTime,
+    required this.deliveryFee,
+  });
+}
--- app/lib/restaurant/view/restaurant_screen.dart
+++ app/lib/restaurant/view/restaurant_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/common/const/api_type.dart';
+import 'package:restaurant_app/common/const/data.dart';
+import 'package:restaurant_app/ex/config.dart';
+import 'package:restaurant_app/ex/dio_ex.dart';
 import 'package:restaurant_app/restaurant/component/restaurant_card.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
+
+  Future<List> paginateRestaurant() async {
+    final res = await dioEx.getRestaurantList(after: "", count: 20);
+
+    return res;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,17 +20,41 @@ class RestaurantScreen extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: RestaurantCard(
-            image: Image.asset(
-              "asset/img/food/ddeok_bok_gi.jpg",
-              fit: BoxFit.cover,
-            ),
-            name: "불타는 떡볶이",
-            tags: ["떡볶이", "치즈", "매운맛"],
-            ratingCount: 100,
-            deliveryTime: 15,
-            deliverFee: 2000,
-            rating: 4.52,
+          child: FutureBuilder<List>(
+            future: paginateRestaurant(),
+            builder: (
+              context,
+              AsyncSnapshot<List> snapshot,
+            ) {
+              if (!snapshot.hasData) {
+                return Container();
+              }
+
+              return ListView.separated(
+                itemBuilder: (_, index) {
+                  final item = snapshot.data![index];
+
+                  return RestaurantCard(
+                    image: Image.network(
+                      "${ip}${item["thumbUrl"]}",
+                      fit: BoxFit.cover,
+                    ),
+                    name: item["name"],
+                    tags: List<String>.from(item["tags"]),
+                    ratingsCount: item["ratingsCount"],
+                    deliveryTime: item["deliveryTime"],
+                    deliveryFee: item["deliveryFee"],
+                    ratings: item["ratings"],
+                  );
+                },
+                separatorBuilder: (_, index) {
+                  return const SizedBox(
+                    height: 16.0,
+                  );
+                },
+                itemCount: snapshot.data!.length,
+              );
+            },
           ),
         ),
       ),
