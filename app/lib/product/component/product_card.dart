2861c496e50ce31b1f23aa41e0568c5806434f27
inajung <ina.jung@coint.co.kr>
2024-04-23 18:31:35 +0900

    feat: 레스토랑 상세 페이지 제품 리스트 카드 위젯 생성

---
+++ app/lib/product/component/product_card.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/common/const/api_type.dart';
+import 'package:restaurant_app/common/const/colors.dart';
+import 'package:restaurant_app/common/const/data.dart';
+
+class ProductCard extends StatelessWidget {
+  final Image image;
+  final String name;
+  final String detail;
+  final int price;
+
+  const ProductCard({
+    super.key,
+    required this.image,
+    required this.name,
+    required this.detail,
+    required this.price,
+  });
+
+  factory ProductCard.fromModel({required RestaurantMenuItem model}) {
+    return ProductCard(
+      image: Image.network(
+        "${ip}${model.imgUrl}",
+        width: 110,
+        height: 110,
+        fit: BoxFit.cover,
+      ),
+      name: model.name,
+      detail: model.detail,
+      price: model.price,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // IntrinsicHeight
+    // 내부 모든 위젯들이 최대 크기를 차지하는 widget 의 height 크기와 동일하게 적용
+    return IntrinsicHeight(
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: image,
+          ),
+          const SizedBox(
+            width: 16.0,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  detail,
+                  // TextOverflow.ellipsis
+                  // ... 처리
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: const TextStyle(
+                    color: BODY_TEXT_COLOR,
+                    fontSize: 14.0,
+                  ),
+                ),
+                Text(
+                  "₩$price",
+                  textAlign: TextAlign.right,
+                  style: const TextStyle(
+                    color: PRIMARY_COLOR,
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
