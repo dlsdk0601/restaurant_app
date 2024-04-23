3b1fc5247a36d2d254ef65f1a2af5c13eee6c4cc
inajung <ina.jung@coint.co.kr>
2024-04-23 18:31:49 +0900

    feat: 레스토랑 상세 screen 생성

---
+++ app/lib/restaurant/view/restaurant_detail_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/common/const/api_type.dart';
+import 'package:restaurant_app/common/layout/default_layout.dart';
+import 'package:restaurant_app/ex/dio_ex.dart';
+import 'package:restaurant_app/product/component/product_card.dart';
+import 'package:restaurant_app/restaurant/component/restaurant_card.dart';
+
+class RestaurantDetailScreen extends StatelessWidget {
+  final String id;
+  const RestaurantDetailScreen({
+    super.key,
+    required this.id,
+  });
+
+  Future<RestaurantShowRes> getRestaurantDetail() async {
+    final res = await dioEx.restaurantShow(id: id);
+
+    return res;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: "불타는 떡볶",
+      child: FutureBuilder<RestaurantShowRes>(
+        future: getRestaurantDetail(),
+        builder: (_, AsyncSnapshot<RestaurantShowRes> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return CustomScrollView(
+            slivers: [
+              renderTop(model: snapshot.data!),
+              renderLabel(),
+              renderProducts(products: snapshot.data!.products),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop({required RestaurantShowRes model}) {
+    return SliverToBoxAdapter(
+      child: RestaurantCard.fromModel(
+        model: model,
+        isDetail: true,
+      ),
+    );
+  }
+
+  SliverPadding renderProducts({required List<RestaurantMenuItem> products}) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            final model = products[index];
+
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard.fromModel(model: model),
+            );
+          },
+          childCount: products.length,
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          "메뉴",
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+}
