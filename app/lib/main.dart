dd6a8b386b72dafcbac229bb6e57b54a3081abe1
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-24 21:49:37 +0900

    feat: 불필요한 import 제거

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:restaurant_app/common/view/splash_screen.dart';
-import 'package:restaurant_app/user/view/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
