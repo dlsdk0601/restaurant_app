ce0eb644265541f0fa5310df2c94578fceb75c80
dlsdk0601 <dlsdk0601@gmail.com>
2024-04-25 00:14:56 +0900

    feat: setTokenStorage 모듈 생성

---
+++ app/lib/ex/data_utils.dart
@@ -0,0 +1,15 @@
+import 'package:restaurant_app/common/const/api_type.dart';
+import 'package:restaurant_app/common/const/data.dart';
+
+class DataUtils {
+  static pathToUrl(String value) {
+    return "${ip}${value}";
+  }
+
+  static Future<void> setTokenStorage(SignInRes res) async {
+    final refreshToken = res.refreshToken;
+    final accessToken = res.accessToken;
+    await storage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+    await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+  }
+}
