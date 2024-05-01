53817498defbc7cc2fdbb929216f75825a582610
dlsdk0601 <dlsdk0601@gmail.com>
2024-05-01 21:29:03 +0900

    feat: 다음 페이지 fetch 를 위해 scrollController 추가

--- app/lib/restaurant/view/restaurant_screen.dart
+++ app/lib/restaurant/view/restaurant_screen.dart
@@ -5,11 +5,38 @@ import 'package:restaurant_app/restaurant/component/restaurant_card.dart';
 import 'package:restaurant_app/restaurant/provider/restaurant_provider.dart';
 import 'package:restaurant_app/restaurant/view/restaurant_detail_screen.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 위치가 최대 길이 보다 덜 되는 위치 까지 왔다면
+    // 다음 페이지 fetch
+
+    // controller.offset => 현재 스크롤
+    // controller.position.maxScrollExtent => 스크롤 할 수 있는 최대 스크롤
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
     // Loading 일 때
@@ -19,13 +46,40 @@ class RestaurantScreen extends ConsumerWidget {
       );
     }
 
+    // error
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    // CursorPagination
+    // CursorPaginationFetchingMore
+    // CursorPaginationRefetching
+    // 3 가지 모두 CursorPagination 를 상속 받아서 CursorPagination 이 기준
     final cp = data as CursorPagination;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
-        itemCount: cp.data.length,
+        controller: controller,
+        // +1 해준 이유는 로딩 때문에 로딩 그릴 한칸의 자리를 더 만든다.
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8.0,
+              ),
+              child: Center(
+                child: data is CursorPaginationFetchingMore
+                    ? const CircularProgressIndicator()
+                    : const Text("마지막 데이터"),
+              ),
+            );
+          }
+
           final item = cp.data[index];
 
           return GestureDetector(
