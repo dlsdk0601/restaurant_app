11c0c898fa747e76a1585c3c9c3b7b7bb98302c4
dlsdk0601 <dlsdk0601@gmail.com>
2024-05-01 18:07:17 +0900

    feat: 제레릭을 기본 PaginationBase 클래스로 수정

--- app/lib/restaurant/provider/restaurant_provider.dart
+++ app/lib/restaurant/provider/restaurant_provider.dart
@@ -3,28 +3,31 @@ import 'package:restaurant_app/common/const/api_type.dart';
 import 'package:restaurant_app/restaurant/repository/restaurant_repository.dart';
 
 final restaurantProvider =
-    StateNotifierProvider<RestaurantStateNotifier, List<RestaurantListResItem>>(
+    StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
   (ref) {
     final repository = ref.watch(restaurantRepositoryProvider);
     final notifier = RestaurantStateNotifier(repository: repository);
+
     return notifier;
   },
 );
 
-class RestaurantStateNotifier
-    extends StateNotifier<List<RestaurantListResItem>> {
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
   final RestaurantRepository repository;
 
   RestaurantStateNotifier({
     required this.repository,
-  }) : super([]) {
+  }) : super(
+          // 처음은 로딩 상태라서 로딩 상태의 class 를 넣는다.
+          CursorPaginationLoading(),
+        ) {
     // 해당 provider 가 생성 될때 바로 paginate 를 실행시켜주기 위해서 한번 호출 한다.
     paginate();
   }
 
   paginate() async {
-    final res = await repository.paginate(after: "", count: 20);
+    final res = await repository.paginate();
 
-    state = res.data;
+    state = res;
   }
 }
