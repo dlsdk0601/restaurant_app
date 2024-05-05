434cdeeaf98a56e20a8cffc72c3b83c973117512
dlsdk0601 <dlsdk0601@gmail.com>
2024-05-05 23:52:35 +0900

    feat: rating model init

--- server/src/main.ts
+++ server/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
-  const port = 3001;
+  const port = 3000;
   console.log(`========== port: ${port} ==========`);
   await app.listen(port);
 }
