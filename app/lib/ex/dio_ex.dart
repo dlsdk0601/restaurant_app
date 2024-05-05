1351ea30bf3e920c615a6b423190429c9dc59922
dlsdk0601 <dlsdk0601@gmail.com>
2024-05-06 01:36:45 +0900

    feat: 개발 단계 동안 테스트를 위해 token print 추가

--- app/lib/ex/dio_ex.dart
+++ app/lib/ex/dio_ex.dart
@@ -36,6 +36,7 @@ class CustomInterceptor extends Interceptor {
       // true 값 상관없이 token 이 있든 없든 일단 보내야하는게 맞다.
       options.headers.remove(accessTokenKey);
       final token = await storage.read(key: ACCESS_TOKEN_KEY);
+      print("[TOKEN] : $token");
       options.headers.addAll(DataUtils.getBearerHeader(token));
     }
     return super.onRequest(options, handler);
