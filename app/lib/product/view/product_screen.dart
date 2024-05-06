8c0bdd0fef6ff1fd2b397932800c91c09dfa9f8d
dlsdk0601 <dlsdk0601@gmail.com>
2024-05-06 22:18:41 +0900

    feat: 상품 목록에서 캐시 처리 추가

--- app/lib/product/view/product_screen.dart
+++ app/lib/product/view/product_screen.dart
@@ -3,6 +3,7 @@ import 'package:restaurant_app/common/component/pagination_list_view.dart';
 import 'package:restaurant_app/common/const/api_type.dart';
 import 'package:restaurant_app/product/component/product_card.dart';
 import 'package:restaurant_app/product/provider/product_provider.dart';
+import 'package:restaurant_app/restaurant/view/restaurant_detail_screen.dart';
 
 class ProductScreen extends StatelessWidget {
   const ProductScreen({super.key});
@@ -12,7 +13,16 @@ class ProductScreen extends StatelessWidget {
     return PaginationListView<ProductModel>(
       provider: productProvider,
       itemBuilder: <ProductModel>(_, index, model) {
-        return ProductCard.fromProductModel(model: model);
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => RestaurantDetailScreen(id: model.restaurant.id),
+              ),
+            );
+          },
+          child: ProductCard.fromProductModel(model: model),
+        );
       },
     );
   }
--- app/lib/restaurant/provider/restaurant_provider.dart
+++ app/lib/restaurant/provider/restaurant_provider.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:restaurant_app/common/const/api_type.dart';
 import 'package:restaurant_app/common/provider/pagination_provider.dart';
@@ -12,7 +13,9 @@ final restaurantDetailProvider =
     return null;
   }
 
-  return state.data.firstWhere((element) => element.id == id);
+  // firstWhere 의 단점은 존재하지 않으면 null 이 아닌 error 를 던진다.
+  // 그래서 collection 에서 orNull 로 수정 해준다.
+  return state.data.firstWhereOrNull((element) => element.id == id);
 });
 
 final restaurantProvider =
@@ -49,11 +52,31 @@ class RestaurantStateNotifier
 
     final res = await repository.getRestaurantDetail(id: id);
 
-    // id 인 데이터는 새로 받은 res 로 그외에는 기존 데이터 e 로
-    state = pState.copyWith(
-      data: pState.data
-          .map<RestaurantListResItem>((e) => e.id == id ? res : e)
-          .toList(),
-    );
+    /*
+    * 경우의 수를 잘 생각 해보자
+    * 1. [ListModel(1), ListModel(2), ListModel(3)]
+    * id 가 2 라면 getDetail 로 2 가져옴
+    * id 가 2인 ListModel(2) 를 DetailModel(2) 로 대치
+    *
+    * 2. 근데 [ListModel(1), ListModel(2), ListModel(3)] 인데,
+    * id 가 5라면, 에러가 난다.
+    * 이럴때는 list 끝에다가 데이터를 추가
+    * [ListModel(1), ListModel(2), ListModel(3), DetailModel(5)]
+    * */
+
+    if (pState.data.where((element) => element.id == id).isEmpty) {
+      // list 에 데이터가 존재하지 않을
+      state = pState.copyWith(
+        data: <RestaurantListResItem>[...pState.data, res],
+      );
+    } else {
+      // list 에 데이터가 이미 존재 할 때
+      // id 인 데이터는 새로 받은 res 로 그외에는 기존 데이터 e 로
+      state = pState.copyWith(
+        data: pState.data
+            .map<RestaurantListResItem>((e) => e.id == id ? res : e)
+            .toList(),
+      );
+    }
   }
 }
