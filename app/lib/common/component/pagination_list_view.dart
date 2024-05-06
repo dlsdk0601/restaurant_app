2e127ac6ec388d5445c3353372ee94af1dcd6991
dlsdk0601 <dlsdk0601@gmail.com>
2024-05-06 21:49:35 +0900

    feat: pagination screen 추상화

---
+++ app/lib/common/component/pagination_list_view.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:restaurant_app/common/const/api_type.dart';
+import 'package:restaurant_app/common/provider/pagination_provider.dart';
+
+import '../../ex/data_utils.dart';
+
+typedef PaginationWidgetBuilder<T extends IModelWithId> = Widget Function(
+  BuildContext context,
+  int index,
+  T model,
+);
+
+class PaginationListView<T extends IModelWithId>
+    extends ConsumerStatefulWidget {
+  final StateNotifierProvider<PaginationProvider, CursorPaginationBase>
+      provider;
+  final PaginationWidgetBuilder<T> itemBuilder;
+
+  const PaginationListView({
+    super.key,
+    required this.provider,
+    required this.itemBuilder,
+  });
+
+  @override
+  ConsumerState<PaginationListView> createState() =>
+      _PaginationListViewState<T>();
+}
+
+class _PaginationListViewState<T extends IModelWithId>
+    extends ConsumerState<PaginationListView> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(listener);
+  }
+
+  void listener() {
+    DataUtils.paginateInit(
+      controller: controller,
+      provider: ref.read(widget.provider.notifier),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(listener);
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(widget.provider);
+
+    // error
+    if (state is CursorPaginationError) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            state.message,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 16.0,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(widget.provider.notifier).paginate(
+                    forceRefetch: true,
+                  );
+            },
+            child: const Text(
+              "다시 시도",
+            ),
+          )
+        ],
+      );
+    }
+
+    // 로딩
+    if (state is CursorPaginationLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    // CursorPagination
+    // CursorPaginationFetchingMore
+    // CursorPaginationRefetching
+    // 3 가지 모두 CursorPagination 를 상속 받아서 CursorPagination 이 기준
+    final cp = state as CursorPagination<T>;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ListView.separated(
+        controller: controller,
+        // +1 해준 이유는 로딩 때문에 로딩 그릴 한칸의 자리를 더 만든다.
+        itemCount: cp.data.length + 1,
+        itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8.0,
+              ),
+              child: Center(
+                child: cp is CursorPaginationFetchingMore
+                    ? const CircularProgressIndicator()
+                    : const Text("마지막 데이터"),
+              ),
+            );
+          }
+
+          final item = cp.data[index];
+
+          return widget.itemBuilder(context, index, item);
+        },
+        separatorBuilder: (_, index) {
+          return const SizedBox(
+            height: 16.0,
+          );
+        },
+      ),
+    );
+  }
+}
