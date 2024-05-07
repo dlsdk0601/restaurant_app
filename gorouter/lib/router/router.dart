322668d0e45128f208a786e085bb595a1366bc5a
inajung <ina.jung@coint.co.kr>
2024-05-07 20:14:04 +0900

    feat: go 함수 예제 생성

--- gorouter/lib/router/router.dart
+++ gorouter/lib/router/router.dart
@@ -1,11 +1,17 @@
 import 'package:go_router/go_router.dart';
+import 'package:gorouter/screens/1_basic_screen.dart';
 import 'package:gorouter/screens/root_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      path: "/",
-      builder: (context, state) => const RootScreen(),
-    ),
+        path: "/",
+        builder: (context, state) => const RootScreen(),
+        routes: [
+          GoRoute(
+            path: "basic",
+            builder: (context, state) => const BasicScreen(),
+          ),
+        ]),
   ],
 );
