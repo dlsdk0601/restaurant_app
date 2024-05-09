98ca321f050ca6f0801d8a8c2c116874aba012ea
inajung <ina.jung@coint.co.kr>
2024-05-09 16:50:43 +0900

    feat: order 와 관련된 model 생성

--- app/lib/common/const/api_type.dart
+++ app/lib/common/const/api_type.dart
@@ -337,3 +337,64 @@ class PatchBasketBodyBasket {
 
   Map<String, dynamic> toJson() => _$PatchBasketBodyBasketToJson(this);
 }
+
+@JsonSerializable()
+class OrderProductModel {
+  final String id;
+  final String name;
+  final String detail;
+  @JsonKey(
+    fromJson: DataUtils.pathToUrl,
+  )
+  final String imgUrl;
+  final int price;
+
+  OrderProductModel({
+    required this.id,
+    required this.name,
+    required this.detail,
+    required this.imgUrl,
+    required this.price,
+  });
+
+  factory OrderProductModel.fromJson(Map<String, dynamic> json) =>
+      _$OrderProductModelFromJson(json);
+}
+
+@JsonSerializable()
+class OrderProductAndCountModel {
+  final OrderProductModel product;
+  final int count;
+
+  OrderProductAndCountModel({
+    required this.product,
+    required this.count,
+  });
+
+  factory OrderProductAndCountModel.fromJson(Map<String, dynamic> json) =>
+      _$OrderProductAndCountModelFromJson(json);
+}
+
+@JsonSerializable()
+class OrderModel implements IModelWithId {
+  @override
+  final String id;
+  final List<OrderProductAndCountModel> products;
+  final int totalPrice;
+  final RestaurantListResItem restaurant;
+  @JsonKey(
+    fromJson: DataUtils.stringToDateTime,
+  )
+  final DateTime createdAt;
+
+  OrderModel({
+    required this.id,
+    required this.products,
+    required this.totalPrice,
+    required this.restaurant,
+    required this.createdAt,
+  });
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) =>
+      _$OrderModelFromJson(json);
+}
