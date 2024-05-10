2ea59e8496dc32f223aa3efd7c3af96ff5e5194c
inajung <ina.jung@coint.co.kr>
2024-05-10 11:48:09 +0900

    feat: paginationListView 추가

--- app/lib/order/view/order_screen.dart
+++ app/lib/order/view/order_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:restaurant_app/common/component/pagination_list_view.dart';
+import 'package:restaurant_app/common/const/api_type.dart';
+import 'package:restaurant_app/order/provider/order_provider.dart';
+
+import '../component/order_card.dart';
 
 class OrderScreen extends ConsumerWidget {
   const OrderScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return PaginationListView<OrderModel>(
+      provider: orderProvider,
+      itemBuilder: <OrderModel>(_, index, model) {
+        return OrderCard.fromModel(model: model);
+      },
+    );
   }
 }
